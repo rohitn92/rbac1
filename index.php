813c68b4bc0068b9abc544a0442c168456966ff4
rohitn92 <cool.rohit.im@gmail.com>
2014-03-21 08:44:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <html>
 <body>
 
-<form action="createDB.php" method="post">
-    Server Name: <input type="text" name="servername">
-    Database Name: <input type="text" name="databasename">
-    User Name: <input type="text" name="username">
-    Password: <input type="text" name="password">
+<form action="login.php" method="post">
+   
+    User ID: <input type="text" name="id"> 
+    </br>
+    Password: <input type="text" name="pass">
     <input type="submit" value="submit" name="send">
+    
 </form>
 
 </body>
