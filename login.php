3544031427a0822a9feafbf2f34340326aa3aafa
rohitn92 <cool.rohit.im@gmail.com>
2014-03-22 01:29:09 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,40 @@
 <?php
+ob_start();
+$host = "sql2.freemysqlhosting.net";
+$database = "sql233894";
+$user = "sql233894";
+$password = "lY4%yP1!";
+$table= "LOGIN";
+// Connect to server and select databse.
+mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
+mysql_select_db("$database")or die("cannot select DB");
 
-echo "HELlo"
+// Define $myusername and $mypassword 
+$myusername=$_POST['id']; 
+$mypassword=$_POST['pass']; 
+
+// To protect MySQL injection (more detail about MySQL injection)
+$myusername = stripslashes($myusername);
+$mypassword = stripslashes($mypassword);
+$myusername = mysql_real_escape_string($myusername);
+$mypassword = mysql_real_escape_string($mypassword);
+$sql="SELECT * FROM $table WHERE username='$myusername' and password='$mypassword'";
+$result=mysql_query($sql);
+
+// Mysql_num_row is counting table row
+$count=mysql_num_rows($result);
+
+// If result matched $myusername and $mypassword, table row must be 1 row
+if($count==1){
+
+// Register $myusername, $mypassword and redirect to file "login_success.php"
+session_register("myusername");
+session_register("mypassword"); 
+header("location:login_success.php");
+}
+else {
+echo "Wrong Username or Password";
+}
+ob_end_flush();
+
 ?>
