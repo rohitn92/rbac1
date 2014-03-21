21d89d9d0e737dc0e9994e516f4139aa77a6739b
rohitn92 <cool.rohit.im@gmail.com>
2014-03-21 08:43:04 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<form action="createDB.php" method="post">
+    Server Name: <input type="text" name="servername">
+    Database Name: <input type="text" name="databasename">
+    User Name: <input type="text" name="username">
+    Password: <input type="text" name="password">
+    <input type="submit" value="submit" name="send">
+</form>
+
+</body>
+</html>
